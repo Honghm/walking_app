bded5723951d51e808ad4e48108b4b2dc8847f09
sMj2m2ykgg <18520155@gm.uit.edu.vn>
2020-06-28 16:46:57 +0700

    update 28/6

--- lib/screens/register_coun_page.dart
+++ lib/screens/register_coun_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -6,13 +7,18 @@ import 'package:walkingapp/provider/user_provider.dart';
 import 'package:walkingapp/screens/login_page.dart';
 import 'package:walkingapp/widgets/loading.dart';
 
+class Gender {
+  const Gender(this.gender);
+
+  final String gender;
+}
 
 class RegisterPageCoun extends StatefulWidget {
-
   String _name;
   String _email;
   String _pass;
   String _phone;
+
   RegisterPageCoun(this._name, this._email, this._pass, this._phone);
 
   @override
@@ -21,12 +27,17 @@ class RegisterPageCoun extends StatefulWidget {
 
 class _RegisterPageStateCoun extends State<RegisterPageCoun> {
   TextEditingController _weightController = new TextEditingController();
-  TextEditingController _genderController = new TextEditingController();
   TextEditingController _heightController = new TextEditingController();
+  String Gender_text;
+
+  Gender selectedGender;
+  List<Gender> genders = <Gender>[const Gender('Nam'), const Gender('Nữ')];
 
   final _key = GlobalKey<ScaffoldState>();
+
   //final AuthService _auth = AuthService();
   String _uid;
+
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context);
@@ -54,9 +65,8 @@ class _RegisterPageStateCoun extends State<RegisterPageCoun> {
                     ],
                     begin: Alignment.topLeft,
                     end: Alignment.bottomRight,
-                  )
-              ),
-              constraints:  BoxConstraints.expand(),
+                  )),
+              constraints: BoxConstraints.expand(),
               child: Container(
                 color: Colors.white.withOpacity(0.4),
                 child: SingleChildScrollView(
@@ -90,84 +100,135 @@ class _RegisterPageStateCoun extends State<RegisterPageCoun> {
                           child: Text(
                             "XIN CHÀO!",
                             style: TextStyle(
-                                fontSize: 22, color: Colors.white, fontWeight: FontWeight.bold),
+                                fontSize: 22,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
                           ),
                         ),
                         Text(
                           "Đăng ký tài khoản mới",
-                          style:
-                          TextStyle(fontSize: 16, color: Colors.white),
+                          style: TextStyle(
+                              fontSize: 16, color: Colors.white),
                         ),
                         //----------Giới tính---------------
                         Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                          child: TextField(
-                            controller: _genderController,
-                            style:
-                            TextStyle(fontSize: 16, color: Colors.black),
-                            decoration: InputDecoration(
-                                labelText: "Giới Tính",
-                                prefixIcon: Container(
-                                    width: 50, child: Icon(Icons.people)),
-                                border: OutlineInputBorder(
-                                    borderSide: BorderSide(
-                                        color: Color(0xffCED0D2), width: 1),
-                                    borderRadius: BorderRadius.all(
-                                        Radius.circular(6)))),
+                          padding:
+                          const EdgeInsets.only(top: 30, bottom: 10),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.black45),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(6),
+                                )),
+                            child: DropdownButtonHideUnderline(
+                              child: DropdownButton<Gender>(
+                                hint: Text("Giới tính"),
+                                value: selectedGender,
+                                //items: _dropdownMenuItems,
+                                onChanged: //onChangeDropdownItem,
+                                    (Gender Value) {
+                                  setState(() {
+                                    selectedGender = Value;
+                                  });
+                                },
+                                items: genders.map((Gender gender) {
+                                  return DropdownMenuItem<Gender>(
+                                    value: gender,
+                                    child: Row(
+                                      children: <Widget>[
+                                        Padding(
+                                          padding:
+                                          const EdgeInsets.all(8.0),
+                                          child: Text(
+                                            gender.gender,
+                                            style: TextStyle(
+                                                color: Colors.black),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+
                           ),
                         ),
 
                         //----------Cân nặng------------
                         TextField(
                           controller: _weightController,
-                          style: TextStyle(fontSize: 18, color: Colors.black),
+                          style: TextStyle(
+                              fontSize: 18, color: Colors.black),
                           decoration: InputDecoration(
                               labelText: "Cân nặng",
                               prefixIcon: Container(
-                                  width: 50, child: Icon(Icons.accessibility)),
+                                  width: 50,
+                                  child: Icon(Icons.accessibility)),
                               border: OutlineInputBorder(
                                   borderSide: BorderSide(
                                       color: Color(0xffCED0D2), width: 1),
-                                  borderRadius:
-                                  BorderRadius.all(Radius.circular(6)))),
+                                  borderRadius: BorderRadius.all(
+                                      Radius.circular(6)))),
                         ),
                         Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                          padding:
+                          const EdgeInsets.fromLTRB(0, 10, 0, 10),
                           child: TextField(
                             controller: _heightController,
-                            style:
-                            TextStyle(fontSize: 16, color: Colors.black),
+                            style: TextStyle(
+                                fontSize: 16, color: Colors.black),
                             decoration: InputDecoration(
                                 labelText: "Chiều cao",
                                 prefixIcon: Container(
-                                    width: 50, child: Icon(Icons.nature_people)),
+                                    width: 50,
+                                    child: Icon(Icons.nature_people)),
                                 border: OutlineInputBorder(
                                     borderSide: BorderSide(
-                                        color: Color(0xffCED0D2), width: 1),
+                                        color: Color(0xffCED0D2),
+                                        width: 1),
                                     borderRadius: BorderRadius.all(
                                         Radius.circular(6)))),
                           ),
                         ),
 
                         Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 30, 0, 20),
+                          padding:
+                          const EdgeInsets.fromLTRB(0, 30, 0, 20),
                           child: SizedBox(
                             width: double.infinity,
                             height: 52,
                             child: RaisedButton(
-                              onPressed:  ()async{
-                                if(user.isMailExist==false)
-                                  user.signUp_Google( widget._name,widget._email, widget._phone,_weightController.text, _heightController.text, _genderController.text);
-                                else{
+                              onPressed: () async {
+                                print(selectedGender.gender);
+                                if (user.isMailExist == false)
+                                  user.signUp_Google(
+                                      widget._name,
+                                      widget._email,
+                                      widget._phone,
+                                      _weightController.text,
+                                      _heightController.text,
+                                      selectedGender.gender);
+                                else {
                                   if (!await user.signUp(
-                                      widget._name,widget._email, widget._pass, widget._phone,_weightController.text, _heightController.text, _genderController.text)) {
-                                    _key.currentState.showSnackBar(SnackBar(
-                                        content: Text("Đăng ký thất bại")));
+                                      widget._name,
+                                      widget._email,
+                                      widget._pass,
+                                      widget._phone,
+                                      _weightController.text,
+                                      _heightController.text,
+                                      selectedGender.gender)) {
+                                    _key.currentState.showSnackBar(
+                                        SnackBar(
+                                            content: Text(
+                                                "Đăng ký thất bại")));
                                     return;
                                   }
                                 }
-                                changeScreenReplacement(context, LoginPage());
-                                },
+                                changeScreenReplacement(
+                                    context, LoginPage());
+                              },
                               child: Text(
                                 "Đăng ký",
                                 style: TextStyle(
@@ -175,12 +236,11 @@ class _RegisterPageStateCoun extends State<RegisterPageCoun> {
                               ),
                               color: Color(0xff3277D8),
                               shape: RoundedRectangleBorder(
-                                  borderRadius:
-                                  BorderRadius.all(Radius.circular(6))),
+                                  borderRadius: BorderRadius.all(
+                                      Radius.circular(6))),
                             ),
                           ),
                         ),
-
                       ],
                     ),
                   ),
